70ed8c62ead7b3db32ed46b6cdd2d01edf1cc958
Isabella Garduño <isabellagardunito@gmail.com>
2025-05-24 17:21:05 -0600

    Memory only one file

--- baby_duck/semantics/memory.go
+++ baby_duck/semantics/memory.go
@@ -1,6 +1,9 @@
 package semantics
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func NewMemorySegment(start, end int) MemorySegment {
 	return MemorySegment{start: start, end: end, next: start}
@@ -39,4 +42,55 @@ func NewMemoryManager() *MemoryManager {
 			Strings: NewMemorySegment(9000, 9999), // Add string constant range
 		},
 	}
+}
+
+var Consts = ConstTable{
+	ints:    []string{},
+	floats:  []string{},
+	strings: []string{}, // Initialize string storage
+}
+
+func GetConstAddress(literal string, tipo string) int {
+	var list *[]string
+	var segment *MemorySegment
+	var addr int
+	var err error
+
+	switch tipo {
+	case "int":
+		list = &Consts.ints
+		segment = &memory.Constant.Ints
+	case "float":
+		list = &Consts.floats
+		segment = &memory.Constant.Floats
+	case "string": // Add string case
+		list = &Consts.strings
+		segment = &memory.Constant.Strings // You'll need to add this to MemoryManager
+	default:
+		panic("Tipo de constante no soportado: " + tipo)
+	}
+
+	// Buscar si ya existe
+	for i, val := range *list {
+		if val == literal {
+			return segment.start + i
+		}
+	}
+
+	// Si no existe, asigna nueva dirección
+	addr, err = segment.GetNext()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Evita registrar direcciones virtuales como constantes
+	if num, err := strconv.Atoi(literal); err == nil {
+		if num >= 1000 && num <= 6999 { // dentro de rangos de variables o temporales
+			return num // ya es una dirección, no registrar
+		}
+	}
+
+	*list = append(*list, literal)
+	return addr
 }
